b28dbb496d352dd7bccca679c002d943fe603213
Tasnim1147 <87118828+Tasnim1147@users.noreply.github.com>
2024-12-23 02:17:25 +0000

    Complete task class

---
+++ python/TaskTracker/task.py
@@ -0,0 +1,39 @@
+from time import strftime, localtime
+from status import TaskState
+
+class Task(object):
+   def __init__(
+         self,
+         id: int,
+         description: str,
+         status: TaskState
+      ) -> None:
+
+      self.id = id
+      self.description = description
+      self.status = status
+      self.createdAt = strftime("%a, %d %b %Y %H:%M", localtime())
+      self.updatedAt = self.createdAt
+
+
+   def markInProgress(self) -> None:
+      self.updatedAt = strftime("%a, %d %b %Y %H:%M", localtime())
+      self.status = TaskState.INPROGRESS
+
+   def markDone(self) -> None:
+      self.updatedAt = strftime("%a, %d %b %Y %H:%M", localtime())
+      self.status = TaskState.DONE
+
+   def setDescription(
+      self, 
+      description: str
+      ) -> None:
+      self.updatedAt = strftime("%a, %d %b %Y %H:%M", localtime())
+      self.description = description
+
+   def __repr__(self) -> str:
+      return f"Task {self.id} ({self.status}): {self.des}"
+
+   
+
+
