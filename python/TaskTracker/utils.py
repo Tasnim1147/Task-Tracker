e94040a9f13878aa636a166891227583162fbe8e
Tasnim1147 <87118828+Tasnim1147@users.noreply.github.com>
2024-12-23 03:53:07 +0000

    Check for existing of file while loading tasks

--- python/TaskTracker/utils.py
+++ python/TaskTracker/utils.py
@@ -1,10 +1,11 @@
-from TaskTracker.task import Task, TaskJSONEncoder
+from TaskTracker.task import Task, TaskJSONEncoder, TaskJSONDecoder
 from json import loads, dumps
-
+import os
 
 def loadTasks(path: str) -> list[Task]:
+    if (not os.path.exists(path)): return []
     with open(path, 'r') as fd:
-        return loads(fd.read())
+        return loads(fd.read(), cls=TaskJSONDecoder)
     
 def dumpTasks(
         path: str, 
