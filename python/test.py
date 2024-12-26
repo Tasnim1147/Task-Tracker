d6e0a39497a494a94968895de91107e658f06977
Tasnim1147 <87118828+Tasnim1147@users.noreply.github.com>
2024-12-26 19:55:58 +0000

    Testing

--- python/test.py
+++ python/test.py
@@ -1,6 +1,7 @@
 import logging
+import time
 from subprocess import PIPE, Popen
-
+import typing
 logging.basicConfig(filename="cli.log",
                     level=logging.INFO)
 
@@ -17,7 +18,59 @@ class TestTaskTracker(object):
                              stderr=PIPE, 
                              text=True)
 
-    def test_add(self): pass
+    def readline(self) -> str:
+        line = ""
+        while self.process.stdout.readable():
+            char = self.process.stdout.read(1)
+            if char == '\n' or not char: break
+            line += char
+            if line == self.prompt: break
+
+        return line
+
+        
+
+
+    def process_io(self,
+                   input_text: str
+                   ) -> str:
+        time.sleep(0.5)
+        out = []
+        self.logger.info("Starting to clean stdout")
+        while True:
+            if (self.process.stdout.readable()):
+                line = self.readline()
+                self.logger.info("Found: " + line)
+                if (line.strip() == self.prompt.strip()): break
+            else:
+                break
+            time.sleep(0.5)
+            print("Continuing to clean stdout stream")
+        self.logger.info(f"Starting to send input: {input_text}")
+        self.process.stdin.write(input_text + '\n')
+        self.process.stdin.flush()
+        self.logger.info(f"Starting to read output")
+        while True:
+            if (self.process.stdout.readable()):
+                out.append(self.readline())
+                if (out[-1].strip() == self.prompt.strip()): 
+                    break
+            else:
+                break
+
+        self.logger.info(f"Input='{input_text}'")
+        self.logger.info(f"Output='{out}'")
+        return out
+
+    def test_add(self): 
+        for task in [
+            "Buy groceries",
+            "Brush teeth",
+            "Eat lunch",
+        ]:
+            command = f"add {task}"
+            self.process_io(input_text=command)
+
     def test_update(self): pass
     def test_delete(self): pass
     def test_mark_done(self): pass
@@ -26,6 +79,10 @@ class TestTaskTracker(object):
     def test_list_done(self): pass
     def test_list_todo(self): pass
     def test_list_in_progress(self): pass
+    def test_exit(self):
+        self.process_io(input_text="exit")
+
+
 
     def test_all(self):
         self.test_add()
@@ -36,6 +93,8 @@ class TestTaskTracker(object):
         self.test_list()
         self.test_list_done()
         self.test_list_in_progress()
+
+        self.process.kill()
 
 
 
